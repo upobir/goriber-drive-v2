69e425dc16bbc93568f8ff0a4ce5bb8478adcefd
Sabbir Rahman <33892003+upobir@users.noreply.github.com>
2025-08-19 23:14:24 +0600

    add file upload area

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const state = {
   files: [],
+  draftFiles: [],
 };
 
 const port = location.port ? `:${location.port}` : "";
@@ -35,27 +36,29 @@ ws.onmessage = (e) => {
 ws.onclose = () => console.log("ws: closed");
 
 const renderFiles = () => {
-  document.querySelector(".file-list-section > .no-file-text")?.remove();
-  document
-    .querySelectorAll(".file-list-section > .file-list > div")
-    .forEach((div) => {
-      div.remove();
-    });
-  const storageSpan = document.querySelector(".storage-info span");
+  const availableFileSection = document.querySelector(
+    "#available-file-section"
+  );
+  availableFileSection.querySelector(".no-file-text")?.remove();
+  availableFileSection.querySelectorAll(".file-list > div").forEach((div) => {
+    div.remove();
+  });
+  const storageSpan = availableFileSection.querySelector(".storage-info span");
   storageSpan.textContent = "0 Bytes";
 
   if (state.files.length == 0) {
-    const noFilesTemplate = document.getElementById(
-      "no-available-files-text"
+    const noFilesTemplate = availableFileSection.querySelector(
+      "#no-available-files-text"
     ).content;
-    document
+    availableFileSection
       .querySelector(".file-list")
       .appendChild(noFilesTemplate.cloneNode(true));
   } else {
     let totalSize = 0;
-    const fileRowTemplate =
-      document.getElementById("available-file-row").content;
-    const fileList = document.querySelector(".file-list");
+    const fileRowTemplate = availableFileSection.querySelector(
+      "#available-file-row"
+    ).content;
+    const fileList = availableFileSection.querySelector(".file-list");
     state.files.forEach((file) => {
       const fileRow = fileRowTemplate.cloneNode(true);
       fileRow.querySelector(".file-row").dataset.fileId = file.id;
@@ -74,6 +77,44 @@ const renderFiles = () => {
   }
 };
 
+const renderDraftFiles = () => {
+  const uploadFileSection = document.getElementById("upload-file-section");
+  const fileListContainer = uploadFileSection.querySelector(".file-list");
+  uploadFileSection.querySelectorAll(".file-list > div").forEach((div) => {
+    div.remove();
+  });
+  const storageSpan = uploadFileSection.querySelector(".storage-info span");
+  storageSpan.textContent = "0 Bytes";
+
+  if (state.draftFiles.length > 0) {
+    totalSize = 0;
+    uploadFileSection.hidden = false;
+
+    const fileRowTemplate = document.getElementById("download-file-row");
+
+    state.draftFiles.forEach((file) => {
+      const fileRow = fileRowTemplate.content.cloneNode(true);
+      const row = fileRow.querySelector(".file-row");
+
+      row.dataset.fileId = file.id;
+
+      fileRow.querySelector(".file-name").textContent = file.name;
+      fileRow.querySelector(".file-size").textContent = getHumanReadableSize(
+        file.size
+      );
+      fileRow.querySelector(".delete-button").dataset.fileId = file.id;
+
+      fileListContainer.appendChild(fileRow);
+
+      totalSize += file.size;
+    });
+
+    storageSpan.textContent = getHumanReadableSize(totalSize);
+  } else {
+    uploadFileSection.hidden = true;
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch("/api/v1/files")
     .then((response) => response.json())
@@ -84,6 +125,21 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((error) => {
       console.error("Error fetching files:", error);
     });
+});
+
+document.getElementById("fileInput").addEventListener("change", () => {
+  const fileInput = document.getElementById("fileInput");
+
+  Array.from(fileInput.files).forEach((file) => {
+    state.draftFiles.push({
+      id: new Date().getTime() + "-" + Math.random().toString(36).substring(6),
+      name: file.name,
+      size: file.size,
+      file: file,
+    });
+  });
+
+  renderDraftFiles();
 });
 
 const getHumanReadableSize = (bytes) => {
@@ -121,6 +177,12 @@ const closeModal = (modalId) => {
 const confirmDelete = (fileId) => {
   deleteFile(fileId);
   closeModal("delete-confirm-modal");
+};
+
+const deleteDraftFile = (fileId) => {
+  debugger;
+  state.draftFiles = state.draftFiles.filter((f) => f.id !== fileId);
+  renderDraftFiles();
 };
 
 const deleteFile = (fileId) => {
